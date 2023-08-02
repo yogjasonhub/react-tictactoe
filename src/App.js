0f2a720fcffa44bef1aae04e455ea1c264ad3c08
yogjasonhub <jasonthaoljt@gmail.com>
2023-08-02 10:37:49 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
 
-
+// this function creates a reusable componant 
+// that represents a square on the board with
+// a button via "onClick" for user interaction/react
 function Square({ value, onSquareClick }) {
   return (
   <button className="square" onClick={onSquareClick}>
@@ -10,18 +12,57 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+// this function determines the winner based on 3 in a row values
+function calculateTheWinner (squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 // Here we are creating a component/reusable code
 // used to render, manage, and update the UI elements
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   function handleClick(i) {
+    if (squares[i] || calculateTheWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    }
+    else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
-
+  const winner = calculateTheWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner + "!!";
+  }
+  else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
   return (
     <React.Fragment>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() =>handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() =>handleClick(1)} />
