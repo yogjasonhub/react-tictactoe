cc14ca749552122dd6fbe138f7cd8e4d74fb2338
yogjasonhub <jasonthaoljt@gmail.com>
2023-08-02 12:09:58 -0500

    App.js Update

--- src/App.js
+++ src/App.js
@@ -64,26 +64,50 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length -1);
   }
 
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((Squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go To Move #' + move;
+    }
+    else {
+      description = 'Restart Game!';
+    }
+
+  return (
+    <li key={move}>
+      <button onClick={() => jumpTo(move)}>{description}</button>
+    </li>
+  );
+  });
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{ moves }</ol>
       </div>
     </div>
-  )
+  );
 }
+
+
+
 
 // this function determines the winner based on 3 in a row values
 function calculateTheWinner (squares) {
