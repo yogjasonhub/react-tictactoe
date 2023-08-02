3e25b641f6806a6de73063a5a5f16830a46a6564
yogjasonhub <jasonthaoljt@gmail.com>
2023-08-02 09:07:55 -0500

    app update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import { useState } from 'react';
 
 // Here we are creating a component/reusable code
 // used to render, manage, and update the UI elements
-export default function Square() {
+export default function Board() {
   return (
     <React.Fragment>
-      <div className="board-row-1">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <div className="board-row">
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
       </div>
-      <div className="board-row-1">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <div className="board-row">
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
       </div>
-      <div className="board-row-1">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <div className="board-row">
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>  
       </div>
     </React.Fragment>
   );
+
+  function Square() {
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+      setValue('X');
+    }
+
+    return (
+    <button className="square"
+      onClick={handleClick}
+    >
+      {value}</button>
+    );
+    }
+
 }
