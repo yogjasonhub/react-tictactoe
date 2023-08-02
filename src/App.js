9b598a3e4ee23a0624518af162f825b175be0968
yogjasonhub <jasonthaoljt@gmail.com>
2023-08-02 08:29:01 -0500

    initial create

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component }  from 'react';
+
+// Here we are creating a component/reusable code
+// used to render, manage, and update the UI elements
+// 
+export default function Square() {
+  return <button className="square">X</button>;
+}
