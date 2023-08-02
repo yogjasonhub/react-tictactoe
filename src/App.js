da9086dd374e5ddae4696c65138591e0ca26f77a
yogjasonhub <jasonthaoljt@gmail.com>
2023-08-02 08:38:16 -0500

    rename

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React, { Component }  from 'react';
+import React from 'react';
 
 // Here we are creating a component/reusable code
 // used to render, manage, and update the UI elements
-// 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <React.Fragment>
+      <div className="board-row-1">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row-1">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row-1">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+    </React.Fragment>
+  );
 }
